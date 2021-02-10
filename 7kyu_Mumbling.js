8a8ae3c13d9f244fa8c61e8dd96e833a5c13c9f0
hadeebataj <61531844+hadeebataj@users.noreply.github.com>
2021-02-10 13:06:32 +0530

    Update 7kyu_Mumbling.js

--- 7kyu_Mumbling.js
+++ 7kyu_Mumbling.js
@@ -1,19 +1,25 @@
 function accum(s) {
-	s = s.toLowerCase();
-  let l = s.length;
-  let letter = '';
-  let finalStr = '';
-  for(let i=0; i<l; i++){
-    letter = s[i];
-    for(let j=0; j<=i; j++){
-      if(j == 0){
-        finalStr += letter.toUpperCase();
-      } else {
-        finalStr += letter;
-      }
-    }
-    finalStr += '-';
-  }
-  finalStr = finalStr.slice(0, -1);
-  return finalStr;
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
 }
+
+
+
+// function accum(s) {
+// 	s = s.toLowerCase();
+//   let l = s.length;
+//   let letter = '';
+//   let finalStr = '';
+//   for(let i=0; i<l; i++){
+//     letter = s[i];
+//     for(let j=0; j<=i; j++){
+//       if(j == 0){
+//         finalStr += letter.toUpperCase();
+//       } else {
+//         finalStr += letter;
+//       }
+//     }
+//     finalStr += '-';
+//   }
+//   finalStr = finalStr.slice(0, -1);
+//   return finalStr;
+// }
